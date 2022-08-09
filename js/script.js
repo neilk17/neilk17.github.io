bc2d8cba10d443e7cd1783593c27afe9641dd757
Neil Kanakia <kanakianeil@gmail.com>
2022-08-09 17:14:03 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const btn = document.querySelector(".btn-toggle");
 const theme = document.querySelector("#theme-link");
+console.log(theme.getAttribute("href"));
 btn.addEventListener("click", function() {
     console.log("click")
-    if (theme.getAttribute("href") == "light-style.css") {
+    if (theme.getAttribute("href") == "stylesheets/light-style.css") {
         theme.href = "stylesheets/dark-style.css";
+        btn.innerHTML = 'Light';
+        console.log("clicked")
     } else {
+        theme.href = "stylesheets/light-style.css";
         btn.innerHTML = 'Dark';
     }
 });
