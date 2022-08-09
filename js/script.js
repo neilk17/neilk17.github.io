b19f01e9c2d6ca96b06c99e38fd169671fd01f4f
Neil Kanakia <kanakianeil@gmail.com>
2022-08-09 16:23:07 +0530

    update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const theme = document.querySelector("#theme-link");
 btn.addEventListener("click", function() {
     console.log("click")
     if (theme.getAttribute("href") == "light-style.css") {
-        theme.href = "dark-style.css";
+        theme.href = "stylesheets/dark-style.css";
     } else {
         btn.innerHTML = 'Dark';
     }
