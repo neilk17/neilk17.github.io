54f6b99bc0ce25ef69fdce107f61d2665fbd1dd1
Neil Kanakia <kanakianeil@gmail.com>
2022-08-10 15:27:26 +0530

    remove console.log

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@ const btn = document.querySelector(".btn-toggle");
 const theme = document.querySelector("#theme-link");
 console.log(theme.getAttribute("href"));
 btn.addEventListener("click", function() {
-    console.log("click")
     if (theme.getAttribute("href") == "stylesheets/light-style.css") {
         theme.href = "stylesheets/dark-style.css";
         btn.innerHTML = 'Light';
-        console.log("clicked")
     } else {
         theme.href = "stylesheets/light-style.css";
         btn.innerHTML = 'Dark';
