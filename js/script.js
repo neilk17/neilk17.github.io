5d2368e136408656746884b75d677aad64b3547e
Neil Kanakia <kanakianeil@gmail.com>
2022-08-11 20:25:52 +0530

    dark mode based on system preference

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+let darkModePreference = window.matchMedia("(prefers-color-scheme: dark)").matches;
 const btn = document.querySelector(".btn-toggle");
 const theme = document.querySelector("#theme-link");
-console.log(theme.getAttribute("href"));
+
+if (darkModePreference) {
+    theme.href = "stylesheets/dark-style.css";
+    btn.innerHTML = 'Light';
+}  else {
+        theme.href = "stylesheets/light-style.css";
+        btn.innerHTML = 'Dark';
+}
+
 btn.addEventListener("click", function() {
     if (theme.getAttribute("href") == "stylesheets/light-style.css") {
         theme.href = "stylesheets/dark-style.css";
