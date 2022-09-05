463e23d59ab67d394aba7b374d4ce407f95be71f
Neil Kanakia <kanakianeil@gmail.com>
2022-09-05 08:51:32 +0530

    delete redundant

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 let darkModePreference = window.matchMedia("(prefers-color-scheme: dark)").matches;
-const btn = document.querySelector(".dark-mode");
+const btn = document.querySelector(".img-button");
 const theme = document.querySelector("#theme-link");
 
 if (darkModePreference) {
-    theme.href = "stylesheets/dark-style.css";
+    theme.href = "stylesheets/dark.css";
 }  else {
-        theme.href = "stylesheets/light-style.css";
+        theme.href = "stylesheets/light.css";
 }
 
 btn.addEventListener("click", function() {
-    if (theme.getAttribute("href") == "stylesheets/light-style.css") {
-        theme.href = "stylesheets/dark-style.css";
+    if (theme.getAttribute("href") == "stylesheets/light1.css") {
+        theme.href = "stylesheets/dark.css";
+    } else if (theme.getAttribute("href") == "stylesheets/light1.css") { theme.href = "stylesheets/light1.css";
+        theme.href = "stylesheets/light2.css";
     } else {
-        theme.href = "stylesheets/light-style.css";
+        theme.href = "stylesheets/light1.css";
     }
 });
